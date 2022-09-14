470d875d378821930011e40d94e6ea78c8830118
vaishnavikumari59 <106106335+vaishnavikumari59@users.noreply.github.com>
2022-09-14 23:28:26 +0530

    ADDITION

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class add {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two number");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int c=n1+n2;
+        System.out.println(c);
+    }
+}
